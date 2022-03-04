baa4408ace334881c6c872aacd0f885b57acaa7b
Jkutkut <jorgeregonzalez@gmail.com>
2022-03-04 08:50:37 +0100

    updated

--- cloneCursus.sh
+++ cloneCursus.sh
@@ -6,35 +6,18 @@ HTTPS_CLONE_START="https://github.com/$USR"
 HTTPS_DOWNLOAD="https://raw.github.com/$USR"
 CLONE_END=".git"
 
-REPO="42Madrid-Cursus"
+REPO="42Madrid-"
 
-TMP_FILE="tmpREADME.md"
+REPOS="Born2beroot
+ft_printf
+get_next_line
+Libft
+pipex
+push_swap
+so_long"
 
-sshMultiClone() {
-	type=$1
-	start=$2
-	end=$3
+for reponame in $REPOS; do
+	echo $SSH_CLONE_START$REPO$repoName$CLONE_END
+	git clone $SSH_CLONE_START$REPO$repoName$CLONE_END $reponame
+done
 
-	for i in $(seq -w $start $end); do
-		repoName=$REPO_PREFIX$type"_"$i
-		echo $SSH_CLONE_START$repoName$CLONE_END
-		git clone $SSH_CLONE_START$repoName$CLONE_END
-	done
-}
-
-# wget $HTTPS_DOWNLOAD/$REPO/master/README.md -O $TMP_FILE --no-verbose
-
-#while read l; do
-#	#if [[ "$l" =~ "## Projects:" ]]; then
-#	if [[ "$l" =~ "\| .* \| .* \| .* \|" ]]; then
-#		echo "-----------------------------------"
-#		echo "$l"
-#		echo "-----------------------------------"
-#	fi
-#done < $TMP_FILE
-
-cat $TMP_FILE | grep "*en.*.pdf*"
-
-#cat $TMP_FILE | grep "<!--" -v | cut -d '|' -f 2 -s | grep "\[" | sed 's/\[(.*)\]\((.*)\)/git clone \2 \1/'
-#cat $TMP_FILE | grep "<!--" -v | cut -d '|' -f 2 -s | grep "\[" 
-#cat $TMP_FILE | grep "<!--" -v | cut -d '|' -f 2 -s | grep "\[" 
